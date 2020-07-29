0db4adc0f4f877a3548935322e0afc479b6ecc2e
Kyle Rudy <krudy@thesoftwareguild.com>
2020-07-29 11:36:48 -0500

    exampe app

--- gametrackerjpa/src/main/java/com/sg/gametrackerjpa/controller/GameController.java
+++ gametrackerjpa/src/main/java/com/sg/gametrackerjpa/controller/GameController.java
@@ -1,18 +1,24 @@
 package com.sg.gametrackerjpa.controller;
 
 import com.sg.gametrackerjpa.model.Game;
+import com.sg.gametrackerjpa.model.Platform;
+import com.sg.gametrackerjpa.model.Publisher;
 import com.sg.gametrackerjpa.repositories.GameRepository;
 import com.sg.gametrackerjpa.repositories.PlatformRepository;
 import com.sg.gametrackerjpa.repositories.PublisherRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author Kyle David Rudy
  */
+@CrossOrigin
 @RestController
 public class GameController {
     
@@ -29,5 +35,20 @@ public class GameController {
     @GetMapping("games")
     public List<Game> getAllGames() {
         return games.findAll();
+    }
+    
+    @GetMapping("platforms")
+    public List<Platform> getPlatforms() {
+        return platforms.findAll();
+    }
+    
+    @PostMapping("platform")
+    public Platform addPlatform(@RequestBody Platform platform) {
+        return platforms.save(platform);
+    }
+    
+    @GetMapping("publishers")
+    public List<Publisher> getPublishers() {
+        return publishers.findAll();
     }
 }
